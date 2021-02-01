4e94d867965e361d7d08741caaa32cd9151084bb
Kaustav Roy <54073521+kaustavr19@users.noreply.github.com>
2021-02-01 11:38:20 +0530

    similar flutter version of the java code

--- Intro to Dart for Java Devs/bicycle.dart
+++ Intro to Dart for Java Devs/bicycle.dart
@@ -6,9 +6,20 @@ class Bicycle {
   Bicycle(this.cadence,this.gear);
   @override
   String toString()=>'Bicycle:$_speed mph';
+  
+  void applyBrake(int decrement) {
+  _speed -= decrement;
+}
+
+void speedUp(int increment) {
+  _speed += increment;
+}
 }
 
 void main() {
   var bike = Bicycle(2, 1);
   print(bike);
 }
+
+
+//see the jave code at(https://docs.oracle.com/javase/tutorial/java/javaOO/variables.html)
