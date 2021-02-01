49fc7e85fd7182177dec5380375e7266ebee12b6
Kaustav Roy <54073521+kaustavr19@users.noreply.github.com>
2021-02-01 11:46:30 +0530

    using the constructor & final code

--- Intro to Dart for Java Devs/rectangle.dart
+++ Intro to Dart for Java Devs/rectangle.dart
@@ -13,4 +13,10 @@ String toString() =>
       'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
 }
 
-main() {} // Included main() to suppress uncaught exception.
+main() {
+
+print(Rectangle(origin: const Point(10, 20), width: 100, height: 200));
+  print(Rectangle(origin: const Point(10, 10)));
+  print(Rectangle(width: 200));
+  print(Rectangle());
+} // Included main() to suppress uncaught exception.
