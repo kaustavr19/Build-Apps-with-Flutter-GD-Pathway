a0e91d502492bad632aa8fa4da4d2100bd8d090d
Kaustav Roy <54073521+kaustavr19@users.noreply.github.com>
2021-02-01 11:45:11 +0530

    improve the output

--- Intro to Dart for Java Devs/rectangle.dart
+++ Intro to Dart for Java Devs/rectangle.dart
@@ -8,6 +8,9 @@ class Rectangle {
     this.origin = const Point(0, 0),         this.width = 0, 
     this.height = 0,
   });
+  @override
+String toString() =>
+      'Origin: (${origin.x}, ${origin.y}), width: $width, height: $height';
 }
 
 main() {} // Included main() to suppress uncaught exception.
