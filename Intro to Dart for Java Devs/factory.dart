5f29f66f638a7024e6bd12893ab9b75e41acb03f
Kaustav Roy <54073521+kaustavr19@users.noreply.github.com>
2021-02-01 11:56:49 +0530

    create a top level function & implement

--- Intro to Dart for Java Devs/factory.dart
+++ Intro to Dart for Java Devs/factory.dart
@@ -1,5 +1,11 @@
 import 'dart:math';
 
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t create $type.';
+}
+
 abstract class Shape {
   num get area;
 }
@@ -17,8 +23,8 @@ class Square implements Shape {
 }
 
 main() {
-  final circle = Circle(2);
-  final square = Square(2);
+  final circle = shapeFactory('circle');
+  final square = shapeFactory('square');
   print(circle.area);
   print(square.area);
 }
